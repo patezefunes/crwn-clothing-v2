48972206e9e3e7a79f9c357423fccae134cd00c6
Patricio Ezequiel Funes <patezefunes@gmail.com>
2022-06-26 20:19:55 -0300

    Added dummy payment form

---
+++ src/components/payment-form/payment-form.component.jsx
@@ -0,0 +1,14 @@
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import { FormContainer, PaymentFormContainer } from "./payment-form.styles";
+
+const PaymentForm = () => {
+  return (
+    <PaymentFormContainer>
+      <FormContainer onSubmit={()=> alert('Payment Sent')}>
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
+      </FormContainer>
+    </PaymentFormContainer>
+  );
+};
+
+export default PaymentForm;
